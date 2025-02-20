db5c485a474f1cc1b558e0bb18d6b8d2a680bae5
Konrad Mańko <59570784+catobjective@users.noreply.github.com>
2025-02-20 15:09:49 +0100

    New features for updateBlacklist.sh
    +Delete address from personal blacklist after successful operation. It helps to keep user's blacklists clean to benefit script speed. With usage of this script there is no need to keep addresses on personal blacklists because emails are blocked by Action 
    +Handle error during address adding to autoblacklist - there is a rare scenario when email with unallowad symbol exists in personal blacklist (like "=")

--- updateBlacklist.sh
+++ updateBlacklist.sh
@@ -1,15 +1,26 @@
 #! /bin/bash
-set -e
+rm ./blacklist.txt
 ogroup_id="$(pmgsh get /config/ruledb/who | jq '.[] | select(.name=="AutoBlacklist")  | .id')"
 if [ -z "${ogroup_id}" ]; then
     echo "ogroup_id cannot be found"
     exit 1
 fi
-
+handle_error() {
+    local user="$1"
+    #your notificator script here, like:
+    #./push_notification --title "PMG Autolist error" --description "Check users  $user  blacklist - there is an error during list creation" --color "0xFF0000"
+    #or:
+    #echo "ERROR: "$user "there is an error during list creation" >> blacklist_log.txt
+}
 pmgsh get quarantine/quarusers | jq .[].mail | grep -v "*@" | tr -d '"' | while read user
 do
   pmgsh get /quarantine/blacklist -pmail $user | jq .[].address | tr -d '"' | while read address
   do
-    pmgsh create /config/ruledb/who/$ogroup_id/email --email $address
+    try="$(pmgsh create /config/ruledb/who/$ogroup_id/email --email $address)"
+    if [[ $? -eq 0 ]]; then
+        pmgsh delete /quarantine/blacklist -pmail $user -address $address
+      else
+        handle_error "$user"
+    fi
   done
 done
