955191ae8adef5a7797cf6d1f37739f191a36992
Konrad Mańko <59570784+catobjective@users.noreply.github.com>
2025-02-20 15:17:37 +0100

    New features for updateWhitelist.sh
    +Delete address from personal whitelist after successful operation. It helps to keep user's whitelists clean to benefit script speed. With usage of this script there is no need to keep addresses on personal whitelist because emails are allowed by Action 
    +Handle error during address adding to autowhitelist - there is a rare scenario when email with unallowad symbol exists in personal blacklist (like "=")

--- updateWhitelist.sh
+++ updateWhitelist.sh
@@ -1,15 +1,25 @@
 #! /bin/bash
-set -e
 ogroup_id="$(pmgsh get /config/ruledb/who | jq '.[] | select(.name=="AutoWhitelist")  | .id')"
 if [ -z "${ogroup_id}" ]; then
     echo "ogroup_id cannot be found"
     exit 1
 fi
-
+handle_error() {
+    local user="$1"
+    #your notificator script here, like:
+    #./push_notification --title "PMG Autolist error" --description "Check users  $user  whitelist - there is an error during list creation" --color "0xFF0000"
+    #or:
+    #echo "ERROR: "$user "there is an error during list creation" >> whitelist_log.txt
+}
 pmgsh get quarantine/quarusers | jq .[].mail | grep -v "*@" | tr -d '"' | while read user
 do
   pmgsh get /quarantine/whitelist -pmail $user | jq .[].address | tr -d '"' | while read address
   do
-    pmgsh create /config/ruledb/who/$ogroup_id/email --email $address
+    try="$(pmgsh create /config/ruledb/who/$ogroup_id/email --email $address)"
+    if [[ $? -eq 0 ]]; then
+        pmgsh delete /quarantine/whitelist -pmail $user -address $address
+      else
+        handle_error "$user"
+    fi
   done
 done
